8c5c541c89f6f32989ad46b563df6de5460b1d3d
Zachary Becker <bitbytebitco@gmail.com>
2022-10-18 02:15:38 -0400

    added working box_blur function [this  takes the mean of the pixels immediately surrounding the central pixel, then uses this as the next value).

--- first_diff.c
+++ first_diff.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define THRESHOLD 20 
+#define THRESHOLD 10 
+#define BLUR_THRESH 255 
 
 void first_diff(unsigned char *buf, int len){
     int i, n, n_1, n_diff; 
@@ -23,6 +24,168 @@ void first_diff(unsigned char *buf, int len){
             }
         }
     }
+}
+
+/*
+
+###
+#0#
+###
+
+*/
+void box_blur(unsigned char *buf2, unsigned char *buf3, int len){
+    unsigned char *buft;
+    buft = (char *)malloc(len * sizeof(char)); // memory for the file being written
+
+    int r,c,i, sum;
+    int count = 1; 
+
+    int offset = 56; // HEADER byte count + 1
+
+    int cols = 512;
+    int rows = 512;
+
+    int byte_i = offset+1; 
+
+    int tl, tm, tr, cl, cen, cr, bl, bm, br, avg, j;
+
+    int buf_i = 0;
+    
+    for(i=0;i<=55;i++){
+        buf3[i] = buf2[i];
+    }
+    for(i=offset;i<=len;i=i+3){
+        buft[buf_i] = buf2[i];
+        buf_i++;
+    }
+    printf("bytes: %d \r\n", buf_i);
+
+    for(r=0;r<rows;r++){ // row
+        //if(r>=1 && r<512){ // avoiding edges for now
+            for(c=0;c<cols;c++){ // cols
+                //if(c>=1 && c<512){ // avoiding edges for now
+            
+                    //if(r == 1 && c<4){
+                    
+                    //printf("\r\nc: %d r: %d \r\n", c, r);
+
+                    if(c==0 && r==0){ // left edge, top row
+                        //printf("1 \r\n");
+                        tl = buft[((r+1)*512)+(c+1)];
+                        tm = buft[((r+1)*512)+(c)];
+                        tr = buft[((r+1)*512)+(c+1)];
+                        cl = buft[((r)*512)+(c+1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c+1)];
+                        bl = buft[((r+1)*512)+(c+1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c+1)];
+                    } else if(c==511 && r==0){ // right edge, top row
+                        //printf("2 \r\n");
+                        tl = buft[((r+1)*512)+(c-1)];
+                        tm = buft[((r+1)*512)+(c)];
+                        tr = buft[((r+1)*512)+(c-1)];
+                        cl = buft[((r)*512)+(c-1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c-1)];
+                        bl = buft[((r+1)*512)+(c-1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c-1)];
+                    } else if(c==0) { // left edge
+                        //printf("3 \r\n");
+                        tl = buft[((r-1)*512)+(c+1)];
+                        tm = buft[((r-1)*512)+(c)];
+                        tr = buft[((r-1)*512)+(c+1)];
+                        cl = buft[((r)*512)+(c+1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c+1)];
+                        bl = buft[((r+1)*512)+(c+1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c+1)];
+                    } else if(c==511){ // right edge
+                        //printf("4 \r\n");
+                        tl = buft[((r-1)*512)+(c-1)];
+                        tm = buft[((r-1)*512)+(c)];
+                        tr = buft[((r-1)*512)+(c-1)];
+                        cl = buft[((r)*512)+(c-1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c-1)];
+                        bl = buft[((r+1)*512)+(c-1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c-1)];
+                    } else if(c==0 && r==511) { // left edge bottom row
+                        //printf("5 \r\n");
+                        tl = buft[((r-1)*512)+(c+1)];
+                        tm = buft[((r-1)*512)+(c)];
+                        tr = buft[((r-1)*512)+(c+1)];
+                        cl = buft[((r)*512)+(c+1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c+1)];
+                        bl = buft[((r-1)*512)+(c+1)];
+                        bm = buft[((r-1)*512)+(c)];
+                        br = buft[((r-1)*512)+(c+1)];
+                    } else if(c==511 && r==511) { // right edge bottom row
+                        //printf("6 \r\n");
+                        tl = buft[((r-1)*512)+(c-1)];
+                        tm = buft[((r-1)*512)+(c)];
+                        tr = buft[((r-1)*512)+(c-1)];
+                        cl = buft[((r)*512)+(c-1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c-1)];
+                        bl = buft[((r-1)*512)+(c-1)];
+                        bm = buft[((r-1)*512)+(c)];
+                        br = buft[((r-1)*512)+(c-1)];
+                    } else if(c!= 0 && c!=511 && r==0){ // top row
+                        //printf("7 \r\n");
+                        tl = buft[((r+1)*512)+(c-1)];
+                        tm = buft[((r+1)*512)+(c)];
+                        tr = buft[((r+1)*512)+(c+1)];
+                        cl = buft[((r)*512)+(c-1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c+1)];
+                        bl = buft[((r+1)*512)+(c-1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c+1)];
+                    } else { // top row
+                        //printf("8 \r\n");
+                        //printf("%d \r\n", ((r-1)*512)+(c-1));
+                        tl = buft[((r-1)*512)+(c-1)];
+                        tm = buft[((r-1)*512)+(c)];
+                        tr = buft[((r-1)*512)+(c+1)];
+                        cl = buft[((r)*512)+(c-1)];
+                        cen = buft[((r)*512)+(c)];
+                        cr = buft[((r)*512)+(c+1)];
+                        bl = buft[((r+1)*512)+(c-1)];
+                        bm = buft[((r+1)*512)+(c)];
+                        br = buft[((r+1)*512)+(c+1)];
+                        //printf("tl: %d \r\n", tm);
+                    }
+
+                    sum = (tl + tm + tr + cl + cen + cr + bl + bm + br);
+                    avg = sum/9;
+                    
+                    for(j=0;j<3;j++){
+                        buf3[byte_i++] = avg;
+
+                        /*if(cen<BLUR_THRESH){
+                            printf("\r\n avg: %d \r\n", avg);
+                            printf("cen: %d \r\n", cen);
+                            printf("row: %d col: %d \r\n", r, c);
+                            printf("current: %d \r\n", cen);
+                            printf("avg: %d \r\n", avg);
+                            printf("buf: %d \r\n", buft[((r)*512)+(c)]);
+                                
+                        } else {
+                            buf3[byte_i++] = cen;
+                        }*/
+                    }
+            }
+            
+           //printf("\r\n");
+        }
+   
+    printf("count: %d\r\n\r\n",count);
+    printf("rows: %d\r\n\r\n", rows);
 }
 
 void grayscale(unsigned char *buf2, unsigned char *buf1, int len){
@@ -61,15 +224,18 @@ void grayscale(unsigned char *buf2, unsigned char *buf1, int len){
 int main(int argc, char* args[])
 {
 
-    FILE *writeptr, *greyptr, *readptr;    
+    FILE *writeptr, *blurptr, *greyptr, *readptr;    
 
     unsigned char *buf1;
     unsigned char *buf2;
+    unsigned char *buf3;
+
     long filelen;
     int i = 0;
 
     readptr = fopen("blackbuck.bmp", "rb");  // Open file to read in binary mode
     greyptr = fopen("greyscale.bmp", "wb");  // Open file to write in binary mode
+    blurptr = fopen("blur.bmp", "wb");  // Open file to write in binary mode
     writeptr = fopen("out.bmp", "wb");  // Open file to write in binary mode
 
     fseek(readptr, 0, SEEK_END);          // Jump to the end of the file
@@ -78,16 +244,20 @@ int main(int argc, char* args[])
 
     buf1 = (char *)malloc(filelen * sizeof(char)); // memory for the file being read
     buf2 = (char *)malloc(filelen * sizeof(char)); // memory for the file being written
+    buf3 = (char *)malloc(filelen * sizeof(char)); // memory for the file being written
 
     fread(buf1, filelen, 1, readptr); // Read in the entire file
 
     grayscale(buf2, buf1, filelen); // convert to grayscale 
 
+    box_blur(buf2, buf3, filelen);
+    fwrite(buf3, filelen, 1, blurptr);  // write blur file
+
     fwrite(buf2, filelen, 1, greyptr);  // write greyscale file
 
-    first_diff(buf2, filelen); // perform first-diff
+    first_diff(buf3, filelen); // perform first-diff
 
-    fwrite(buf2, filelen, 1, writeptr); // output first-diff results to file
+    fwrite(buf3, filelen, 1, writeptr); // output first-diff results to file
 
     fclose(readptr); // Close file being read 
     fclose(writeptr); // Close file being written
